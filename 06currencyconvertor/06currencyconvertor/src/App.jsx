56f1c8f8864a01f939176193ec425c2cc9c00b70
Shubham Ryan <140340367+imshubhryan@users.noreply.github.com>
2024-07-12 11:09:45 +0530

    .

---
+++ 06currencyconvertor/06currencyconvertor/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import { InputBox } from './components'
+import useCurrencyInfo  from './hooks/useCurrencyInfo'
+
+
+function App() {
+
+  const [amount, setAmount]
+  
+
+  return (
+    <>
+      <h1 className='text-3xl bg-orange-500 '>Currency App With Coffee</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 08minicontext/08minicontext/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Login from './Components/Login'
+import Profile from './Components/Profile'
+import UserContextProvider from './context/UserContextProvider'
+
+
+function App() {
+  
+  return (
+    <UserContextProvider >
+   
+    <h1>React with Coffee</h1>
+    <Login />
+    <Profile />
+    </UserContextProvider>
+    
+  )
+}
+
+export default App
